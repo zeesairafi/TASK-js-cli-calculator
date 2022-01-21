c216fbfc4ce70a4da161aade6a6e06403eb669ad
zeesairafi <alsairafizainab19@gmail.com>
2022-01-21 21:29:01 +0300

    done

--- calculator.js
+++ calculator.js
@@ -4,3 +4,27 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 // Run the code to see how to use prompt.
 const name = prompt("What's your name? ");
 console.log(`Hi, I'm ${name}.`);
+const number1 = prompt("Type the first number:");
+const number2 = prompt("Type the second number:");
+const operation = prompt("Type the opperation: ");
+let result;
+if(isNaN(number1) || isNaN(number2) ){
+console.log("the operation is not valid ");
+}
+else if(operation=="+"){
+    result=number1+ +number2;
+    console.log(number1, "+", number2, "=", result);
+}
+else if(operation=="-"){
+    result=number1- +number2;
+    console.log(number1, "-", number2, "=", result);
+}
+else if(operation=="*"){
+    result=number1* +number2;
+    console.log(number1, "*", number2, "=", result);
+}
+else if(operation=="/"){
+    result=number1/ +number2;
+    console.log(number1, "/", number2, "=", result);
+}
+
